e542fc5e651ffdf01fa48a97324b794e0f8d0146
Jonathan Wright <devorakwright@gmail.com>
2023-03-11 13:14:36 +0200

    Responsiveness added to SPA1. Readme file updated with temp URL.

--- src/components/spa1.tsx
+++ src/components/spa1.tsx
@@ -5,21 +5,26 @@ import {
     VStack,
     HStack,
     Text,
-    Button,
     Link,
+    Image,
+    Button,
+    AspectRatio,
     Center,
 } from '@chakra-ui/react';
 import {
     SocialIcon,
 } from 'react-social-icons';
+import {
+    ArrowSquareDown,
+} from 'iconsax-react';
 
 import Spa1Image from '../branding/Spa1Image.jpg';
 import FpcLogoWhiteGun from '../branding/FpcLogoWhiteGun.png';
 
 const Spa1 = () => (
     <Grid
-        templateRows="repeat(7, 1fr)"
-        templateColumns="repeat(10, 1fr)"
+        templateRows="repeat(6, 1fr)"
+        templateColumns="repeat(6, 1fr)"
         h="calc(100vh)"
         w="100%"
     >
@@ -28,22 +33,34 @@ const Spa1 = () => (
             rowStart={1}
             colEnd={11}
             rowEnd={8}
-            padding="0px"
         >
-            <img src={Spa1Image} alt="FPC page 1 background image" />
-            
+            <img src={Spa1Image} alt="FPC page 1 background" />
         </GridItem>
+        <GridItem 
+            rowStart={1}
+            h={{base: "20px", sm: "20px", md: "20px", lg: "20px", xl: "20px", }}
+        />
         <GridItem
-            rowStart={2}
+            rowStart={{base: 2,}}
             colStart={2}
         >
-            <img src={FpcLogoWhiteGun} alt="FPC logo .PNG" />
+            <AspectRatio
+                maxW="150px"
+                ratio={4 / 3}
+            >
+                <Image
+                    src={FpcLogoWhiteGun}
+                    alt="FPC logo .PNG"
+                />
+            </AspectRatio>
         </GridItem>
         <GridItem
             rowStart={2}
-            colStart={9}
+            colStart={5}
         >
-            <HStack>
+            <HStack
+                justifyContent="right"
+            >
                 <Link href="https://www.facebook.com/profile.php?id=100086423772401">
                     <SocialIcon network="facebook" bgColor="white" style={{ height: 30, width: 30 }} />
                 </Link>
@@ -56,44 +73,74 @@ const Spa1 = () => (
             </HStack>
         </GridItem>
         <GridItem
-            rowStart={{base: 3}}
-            rowSpan={2}
-            colStart={3}
-            colSpan={6}
+            rowStart={3}
+            colStart={{base: 3, sm: 3, md: 2, lg: 2,}}
+            colSpan={{base: 2, sm: 2, md: 4, lg: 4,}}
         >
-            <Center>
-                <VStack>
-                    <Heading
-                        color="#f1564a"
-                        fontFamily="BouchersSans2"
-                        fontSize="150px"
-                    >
-                        Citizens.
-                    </Heading>
-                    <Heading
-                        color="white"
-                        fontFamily="BouchersSans2"
-                        fontSize="150px"
-                    >
-                        Not Subjects.
-                    </Heading>
-                    <Heading
-                        color="white"
-                        fontFamily="circularBold"
-                    >
-                        Welcome to the Firearms Policy Campaign
-                    </Heading>
-                    <Text
-                        color="white"
-                        align="center"
-                        fontSize="30px"
-                    >
-                        As an unapologetic, dynamic, and energetic pro-firearm
-                        entity, we aim to become the go-to source for firearm policy
-                        in South Africa. Firearm owner or not, you can be the 'campaign'
-                        in the Firearms Policy Campaign.
-                    </Text>
-                </VStack>
+            <VStack
+                alignContent="middle"
+            >
+                <Heading
+                    textAlign="center"
+                    color="#f1564a"
+                    fontFamily="BouchersSans2"
+                    fontSize={{
+                        base: "25", sm: "25px", md: "50", lg: "80", xl: "120px",
+                    }}
+                >
+                    Citizens.
+                </Heading>
+                <Heading
+                    textAlign="center"
+                    color="white"
+                    fontFamily="BouchersSans2"
+                    fontSize={{
+                        base: "25", sm: "25px", md: "50", lg: "80px", xl: "120px",
+                    }}
+                >
+                    Not Subjects.
+                </Heading>
+            </VStack>
+        </GridItem>
+        <GridItem
+            rowStart={{base: 5, sm: 4, md: 5, lg: 5, xl: 5,}}
+            colStart={2}
+            colEnd={6}
+        >
+            <VStack>
+                <Heading
+                    color="white"
+                    fontFamily="circularBold"
+                    textAlign="center"
+                    fontSize={{ base: "10px", sm: "10px", md: "20px", lg: "30px", }}
+                >
+                    Welcome to the Firearms Policy Campaign
+                </Heading>
+                <Text
+                    color="white"
+                    textAlign="center"
+                    fontSize={{ base: "30px", sm: "10px", md: "20px", lg: "30px", }}
+                >
+                    As an unapologetic, dynamic, and energetic pro-firearm
+                    entity, we aim to become the go-to source for firearm policy
+                    in South Africa. Firearm owner or not, you can be the 'campaign'
+                    in the Firearms Policy Campaign.
+                </Text>
+            </VStack>
+        </GridItem>
+        <GridItem
+            rowStart={6}
+            colStart={4}
+        >
+            <Center
+                h="100%"
+            >
+                <Button
+                    size="15px"
+                    color="white"
+                >
+                    <ArrowSquareDown size="32" color="grey" />
+                </Button>
             </Center>
         </GridItem>
     </Grid>
--- src/components/spa3.tsx
+++ src/components/spa3.tsx
@@ -7,6 +7,7 @@ import {
     VStack,
     Button,
     Link,
+    Center,
 } from '@chakra-ui/react';
 import {
     SocialIcon,
@@ -17,6 +18,7 @@ const Spa1 = () => (
     <Grid
         templateRows="repeat(6, 1fr)"
         templateColumns="repeat(6, 1fr)"
+        gap={3}
         h="calc(100vh)"
         w="100%"
         bgColor="black"
@@ -27,91 +29,146 @@ const Spa1 = () => (
             colEnd={7}
             rowEnd={3}
         >
-            <img src={Spa3Image} alt="FPC page 1 background image" />
-        </GridItem>
-        <GridItem
-            rowStart={2}
-            colStart={2}
-            colSpan={2}
-        >
-            <VStack
-                align="left"
-            >
-                <Heading
-                    as="b"
-                    color="white"
-                    fontFamily="BouchersSans2"
-                >
-                    ABOUT
-                </Heading>
-                <Heading
-                    color="#f1564a"
-                    fontFamily="BouchersSans2"
-                >
-                    FIREARMS POLICY
-                </Heading>
-                <Heading
-                    color="#f1564a"
-                    fontFamily="BouchersSans2"
-                >
-                    CAMPAIGN
-                </Heading>
-            </VStack>
+            <img src={Spa3Image} alt="FPC page 1 background" />
         </GridItem>
         <GridItem
             rowStart={3}
+            rowEnd={3}
             colStart={2}
-            colSpan={2}
+            colEnd={6}
         >
-            <Text
-                color="white"
-                fontFamily="circularMedium"
+            <Grid
+                templateRows="repeat(2, 1fr)"
+                templateColumns="repeat(2, 1fr)"
+                gap={10}
             >
-                The FPC is unapologetic about firearm ownership,
-                and is committed to increasing firearm choice and
-                proliferation for lawful purposes. We aspire to be
-                creating a broader and more vibrant firearm owning
-                community, backed by healthy and dynamic industry
-                partners.
-            </Text>
-            <Text
-                color="white"
-                fontFamily="circularBook"
-            >
-                The FPC consists of a board of directors to guide
-                and mold the organisation into a sustainable entity,
-                and an non-executive committee to facilitate grassroots
-                initiatives and efforts as a local level.
-            </Text>
+                <GridItem
+                    rowStart={1}
+                    colStart={1}
+                >
+                    <VStack
+                        align="left"
+                    >
+                        <Heading
+                            as="b"
+                            color="white"
+                            fontFamily="BouchersSans2"
+                        >
+                            ABOUT
+                        </Heading>
+                        <Heading
+                            color="#f1564a"
+                            fontFamily="BouchersSans2"
+                        >
+                            FIREARMS POLICY
+                        </Heading>
+                        <Heading
+                            color="#f1564a"
+                            fontFamily="BouchersSans2"
+                        >
+                            CAMPAIGN
+                        </Heading>
+                    </VStack>
+                </GridItem>
+                <GridItem
+                    rowStart={2}
+                >
+                    <Text
+                        color="white"
+                        fontFamily="circularBold"
+                        align="justify"
+                    >
+                        The FPC is unapologetic about firearm ownership,
+                        and is committed to increasing firearm choice and
+                        proliferation for lawful purposes. We aspire to be
+                        creating a broader and more vibrant firearm owning
+                        community, backed by healthy and dynamic industry
+                        partners.
+                    </Text>
+                    <Text
+                        color="white"
+                        fontFamily="circularBook"
+                        align="justify"
+                    >
+                        The FPC consists of a board of directors to guide
+                        and mold the organisation into a sustainable entity,
+                        and an non-executive committee to facilitate grassroots
+                        initiatives and efforts as a local level.
+                    </Text>
+                </GridItem>
+                <GridItem
+                    colStart={2}
+                    rowStart={1}
+                    rowEnd={4}
+                >
+                    <Grid
+                        templateRows="repeat(3, 1fr)"
+                        templateColumns="repeat(1, 1fr)"
+                        bgColor="white"
+                        borderRadius="20px"
+                    >
+                        <GridItem>
+                            <Text
+                                fontSize="20px"
+                                color="#f1564a"
+                                fontFamily="circularBold"
+                                marginLeft="10px"
+                            >
+                                Contact us today
+                            </Text>
+                            <Text
+                                fontSize="15px"
+                                color="black"
+                                fontFamily="circularBook"
+                                marginLeft="10px"
+                            >
+                                Contact the FPC with any queries or feedback that you may have
+                            </Text>
+                            <Text
+                                fontSize="15px"
+                                color="black"
+                                fontFamily="circularBook"
+                                marginLeft="10px"
+                            >
+                                Email us at: admin@fpc.org.za
+                            </Text>
+                        </GridItem>
+                        <GridItem
+                            rowStart={2}
+                            
+                        >
+                            <Text
+                                fontSize="15px"
+                                color="black"
+                                fontFamily="circularBook"
+                                margin="10px"
+                                align="justify"
+                            >
+                                The FPC community on our Facebook Group has a wealth of information
+                                and knowledge! The admins are active and respond to most, if not
+                                every, post! You can search the group on Facebook, or access it
+                                through the FPC Facebook Page. The Facebook social media button on
+                                this website will take you the Page.
+                            </Text>
+                        </GridItem>
+                    </Grid>
+                </GridItem>
+            </Grid>
         </GridItem>
         <GridItem
             rowStart={5}
-            colStart={2}
+            colStart={1}
             colSpan={6}
         >
-            <Heading
-                color="white"
-                fontFamily="circularBold"
-                fontSize="24px"
-            >
-                Proud sponsors and supporters of the Firearms Policy Campaign
-            </Heading>
-        </GridItem>
-        <GridItem
-            colStart={4}
-            colSpan={2}
-            rowStart={2}
-            rowSpan={2}
-            borderRadius="20px"
-            bgColor="white"
-        >
-            <Text
-                fontSize="20px"
-                color="#f1564a"
-                fontFamily="circularBold"
-            >
-                Contact us today
-            </Text>
+            <Center>
+                <Heading
+                    color="white"
+                    fontFamily="circularBold"
+                    fontSize="24px"
+                >
+                    Proud sponsors and supporters of the Firearms Policy Campaign
+                </Heading>
+            </Center>
         </GridItem>
         <GridItem
             rowStart={6}
@@ -164,16 +221,6 @@ const Spa1 = () => (
                 Registration number: K2022754299
             </Text>
         </GridItem>
-        {/* <GridItem
-            rowStart={7}
-            colStart={4}
-        >
-            <Text
-                color="white"
-            >
-                Firearms Policy Campaign NPC
-            </Text>
-        </GridItem> */}
         <GridItem
             rowStart={7}
             colStart={5}
